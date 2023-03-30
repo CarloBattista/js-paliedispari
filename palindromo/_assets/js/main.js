64acf0d5dae82ba19b193ea334ede7c1ae272c05
CarloBattista <carlitobatti@gmail.com>
2023-03-30 15:04:09 +0200

    verifico se la parola inserita è uguale alla parola originale

--- palindromo/_assets/js/main.js
+++ palindromo/_assets/js/main.js
@@ -5,3 +5,19 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 
 */
+
+document.querySelector(".btnSubmit").addEventListener("click", function () { // Al click del bottone
+
+    const container = document.querySelector(".container_result"); // Seleziono nel dom il container
+
+    const inputText = document.querySelector(".inputText").value; // Seleziono nel dom l'input text
+    console.log(inputText)
+
+    const inputLowerCase = inputText.toLowerCase().replace(/ /g, ""); // Converto la parola in minuscolo e rimuovo gli spazi
+    console.log(inputLowerCase)
+
+    // Verifico se la parola invertita è uguale alla parola originale
+    let isPalindroma = true;
+    console.log(isPalindroma)
+
+})
